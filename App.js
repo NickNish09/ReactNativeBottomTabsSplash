6040c7fb654f97bdac654ed7490241e86705ed35
ACSouzaJr <junioracs2000@hotmail.com>
2019-02-01 11:09:10 -0200

    Stack navigation not working

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import { Navigation } from 'react-native-navigation';
-import Home from './src/screens/Home';
+import { Navigation } from "react-native-navigation";
+import Home from "./src/screens/Home";
+import Welcome from "./src/screens/Authentication/Welcome";
+import { SignIn } from "./src/screens/Authentication/SignIn";
+import { SignUp } from "./src/screens/Authentication/SignUp";
 
 Navigation.registerComponentWithRedux(`Home`, () => Home);
-
-
+Navigation.registerComponentWithRedux("Welcome", () => Welcome);
+Navigation.registerComponentWithRedux("SignIn", () => SignIn);
+Navigation.registerComponentWithRedux("SignUp", () => SignUp);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
     root: {
-      component:{
-        name: "Home"
+      stack: {
+        children: [
+          {
+            component: {
+              name: "Welcome"
+            }
+          }
+        ]
       }
     }
-  })
+  });
 });
---
+++ src/screens/Authentication/Welcome.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { Navigation } from "react-native-navigation";
+
+export default class WelcomeScreen extends Component {
+  goToScreen = screenName => {
+    Navigation.push(this.props.componentId, {
+      component: {
+        name: screenName,
+        options: {
+          topBar: {
+            title: {
+              text: "Pushed screen title"
+            }
+          }
+        }
+      }
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>WelcomeScreen</Text>
+        <Button title="Sign In" onPress={() => this.goToScreen("SignIn")} />
+        <Button title="Sign Up" onPress={() => this.goToScreen("SignUp")} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
