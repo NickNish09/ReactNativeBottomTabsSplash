9456f0d7320dc34e746e342bb81b1ae671dedd7a
ACSouzaJr <junioracs2000@hotmail.com>
2019-02-01 16:02:29 -0200

    feature : timeInterval splashScreeen

--- src/screens/Authentication/Welcome.js
+++ src/screens/Authentication/Welcome.js
@@ -8,7 +8,9 @@ export default class WelcomeScreen extends Component {
   componentDidMount() {
     // do stuff while splash screen is shown
     // After having done stuff (such as async tasks) hide the splash screen
-    SplashScreen.hide();
+    setInterval(() => {
+      SplashScreen.hide();
+    }, 3000);
   }
 
   goToScreen = screenName => {
