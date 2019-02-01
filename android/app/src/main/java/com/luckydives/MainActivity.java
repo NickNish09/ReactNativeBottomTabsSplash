224221027067658080c7191d83ce436d355e4c81
ACSouzaJr <junioracs2000@hotmail.com>
2019-02-01 15:13:35 -0200

    setup

--- android/app/src/main/java/com/luckydives/MainActivity.java
+++ android/app/src/main/java/com/luckydives/MainActivity.java
@@ -1,4 +1,8 @@
 package com.luckydives;
+import android.os.Bundle; // here
+import com.facebook.react.ReactActivity;
+// react-native-splash-screen >= 0.3.1
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 import com.reactnativenavigation.NavigationActivity;
 
@@ -8,5 +12,10 @@ public class MainActivity extends NavigationActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
 
 }
--- src/screens/Authentication/Welcome.js
+++ src/screens/Authentication/Welcome.js
@@ -2,7 +2,15 @@ import React, { Component } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { Navigation } from "react-native-navigation";
 
+import SplashScreen from "react-native-splash-screen";
+
 export default class WelcomeScreen extends Component {
+  componentDidMount() {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }
+
   goToScreen = screenName => {
     Navigation.push(this.props.componentId, {
       component: {
